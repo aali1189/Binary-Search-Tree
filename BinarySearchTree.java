5245533004474007d2a7d73f98a156da8b972585
aali1189 <33586588+aali1189@users.noreply.github.com>
2019-03-26 16:28:02 +0800

    Add files via upload

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,100 +1,227 @@
-
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.List;
+/**
+ *
+ * @author USER
+ */
 public class BinarySearchTree {
-    private static Scanner sc = new Scanner(System.in);
-    static ArrayList<Integer> nodes = new ArrayList<Integer>();
-
+    static List<String> BTS = new ArrayList<>();
+    static ArrayList<Integer> arr = new ArrayList<Integer>();
+    static int[] ard;//Array for DATA
+    static int s = 0;
+    static Scanner scan = new Scanner(System.in);
+    
+    /**
+     * @param args the command line arguments
+     */
+    
+    static void level(int lvl) { 
+        int l = (int) Math.pow(2, lvl);
+        for (int i = 1; i <= l; i++) {
+            BTS.add(null);
+        }
+    }
    
-    static void display() {
-    	 for(int i =0; i<nodes.size();i++) {
-         	if(i==nodes.size()-1) {
-         		System.out.print(nodes.get(i));
-         	}else {
-         		System.out.print(nodes.get(i) + ",");
-         	}
-         	
-         }	
-    	 System.out.println();
-    	option();
-    }
     
-    static void insert() {
-    	System.out.println("\nInsert a node: ");
-        nodes.add(sc.nextInt());
-        System.out.println("THE ELEMENTS IN THE BINARY TREE ARE: ");
-        display();	
-    }
-    
-    static void delete() {
-    	 System.out.println("Delete a node: ");
-         int target = sc.nextInt();
-         for(int i =0; i<nodes.size();i++) {
-         	if(target == nodes.get(i)) {
-         		nodes.remove(i);
-         		break;
-         	}
-         } 
-         System.out.println("THE ELEMENTS IN THE BINARY TREE ARE: ");
-         display();
-    }
-    
-    
-    static void search() {
-    	
-    	 System.out.println("Search a node: ");
-         int target = sc.nextInt();
-         for(int i =0; i<nodes.size();i++) {
-         	if(target == nodes.get(i)) {
-         		 System.out.print("\nTHE ELEMENT IN THE BINARY TREE IS: " + nodes.get(i));
-         	}else {
-        		 System.out.print("THE ELEMENT IN THE BINARY TREE IS NOT FOUND\n");
-             break;
-         	}
-         }
-         System.out.println("THE ELEMENTS IN THE BINARY TREE ARE: ");
-         display();
-    	
-    }
-    
-    
-    static void option() {
-        System.out.println("INSERT[1], DELETE[2], SEARCH[3], EXIT[4]");
-        int input = sc.nextInt();
-            switch (input) {
-                case 1:
-                    insert();
-                    break;
-                case 2:
-                   delete();
-                    break;
-                case 3:
-                	search();
-                   break;
-                   
-                case 4:
-                	break;
-                	
-                	
-                	default:
-                		option();
-                		break;
-            }
-        }
-    public static void main(String[] args) {
-        BinarySearchTree tree = new BinarySearchTree();
-        System.out.println("BINARY SEARCH TREE \n" +
-                "Enter a series of nodes: ");
+    static int newnd(int nd, int inc){
         
-       
-       for(int i =0; i<8; i++) {
-    	   System.out.print((i+1)+" : ");
-    	   nodes.add(sc.nextInt());
-       }
-        System.out.println("The nodes in the binary Search tree are: ");
-        display();
-        option();
-        System.out.println();
-
-     
-        }
+        int nod = (nd*2) + inc;
+        return nod;
+        
     }
+    
+    
+    static void insertion() {
+    	BTS.clear();
+    	int size = ard.length+1;
+    	s= size;
+    	ard = new int[size];    	
+for(int i =0; i<arr.size();i++) {
+    		ard[i] = arr.get(i);
+    	}
+    	System.out.print("\nEnter the element : ");
+    	ard[(ard.length-1)]= scan.nextInt();
+    	
+    	process();
+    	
+    	
+    
+    }
+    
+    
+    static void deletion() {
+    	BTS.clear();
+    	int size = ard.length-1;
+    	s= size;
+    	System.out.print("\nEnter the element : ");
+    	int num = scan.nextInt();
+for(int i =0; i<arr.size();i++) {
+    		if(num == arr.get(i)) {
+    			arr.remove(i);
+    			break;
+    		}
+    	    	}
+    	
+    	ard = new int[size];
+    	
+for(int i =0; i<arr.size();i++) {
+    		ard[i] = arr.get(i);
+    	}
+        
+    	process();
+    	
+    	
+    
+    }
+    
+    
+    
+    public static void main(String[] args) {
+        //declaration of variables
+       
+        
+        //Number of input
+        System.out.print("Enter Array Size : ");
+        s = scan.nextInt();
+        
+        //Reinitialization of array for DATA
+        ard = new int[s];
+        
+        //Input for the data
+        
+        for (int i = 0; i < s; i++) {
+        	System.out.print("Enter Array Element "+(i+1)+" : ");  
+        	int num = scan.nextInt();
+           ard[i] = num;
+          arr.add(num);
+        }
+        
+        //Display of INPUT
+       System.out.print("\n The inputs are: [");
+        
+        for(int i = 0; i < s; i++){
+            if(i == s-1) {
+           System.out.print(ard[i]);
+            }else {
+            	System.out.print(ard[i] + ", ");
+            }
+           
+       }
+        System.out.print("]");
+        //Insertion of DATA to BTS
+       
+       
+       
+       process();        
+    }
+    
+    
+    static void options() {
+        System.out.println("\nInsert[1] Delete[2] Exit[3]");
+    	int op = scan.nextInt();
+    	switch(op) {
+    	
+    	case 1:
+    		insertion();
+    		break;
+    		
+    	case 2:
+    		deletion();
+    		break;
+    		
+    	case 3:
+    		System.exit(0);
+    		break;
+    		
+    		default:
+    			break;
+    	
+    	}
+    }
+    
+    
+    static void process() {
+    	 int lvl,nd;
+         String el;
+         lvl = 0;
+    	 for (int i = 0; i < s; i++) {
+             el = String.valueOf(ard[i]);
+             nd = 0;
+             
+             //Checking if BTS is Empty
+             
+             if (BTS.isEmpty()){
+                 //If it is empty put the first value in the first node(Root Node)
+                 BTS.add(el);
+                 
+                 
+             }else{
+                 
+                 //Infinite loop for comparing
+                 while(true){
+                     
+                     //Comparing
+                     if (ard[i] <= Integer.parseInt(BTS.get(nd))) {//Value is LESS THAN the node
+                         
+                         nd = newnd(nd,1);//Getting new node
+                         
+                         if (nd >= BTS.size()) {
+                             //Adding new levels for the tree
+                             lvl ++;
+                             level(lvl);
+                             
+                         
+                         }
+                         
+                         if (BTS.get(nd) == null) {//If the node is a null then put the value else continue to compare
+                             
+                             BTS.set(nd, el);
+                             break;
+                             
+                         }
+                         
+                     }else {//value is GREATER THAN the node
+                         
+                         nd = newnd(nd,2);
+                         
+                         if (nd >= BTS.size()) {   
+                             lvl ++;
+                             level(lvl);     
+                         }
+                         
+                         if (BTS.get(nd) == null) {
+                             
+                             BTS.set(nd, el);
+                             break;
+                             
+                         }
+                     }
+                     
+                 } //  End comparing
+                 
+             }
+                  
+         }
+    	 display();
+    }
+    
+    
+    static void display() {
+    	 
+        //Display of the BTS
+        System.out.print("\n This is the array: \n");
+        for (int i = 0; i < BTS.size(); i++) {
+        	if(i==BTS.size()-1) {
+                System.out.print(BTS.get(i) + "(" + i +")\n");
+
+        	}else {
+            System.out.print(BTS.get(i) + "(" + i +")" + ", ");
+        	}
+        }
+        
+        options();
+    }
+    
+    
+}
